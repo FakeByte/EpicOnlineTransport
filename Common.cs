53d610b28b346d5ce4d1f4b7d1c77e2d3591220d
Justin Nolan <justin@nolan.de>
2021-01-12 09:19:14 +0800

    Fixed invalid Socket ID

--- Common.cs
+++ Common.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 namespace EpicTransport {
     public abstract class Common {
 
-        public const string SOCKET_ID = "SOCKET_ID";
+        public const string SOCKET_ID = "SOCKETID";
 
         private PacketReliability[] channels;
         private int internal_ch => channels.Length;
